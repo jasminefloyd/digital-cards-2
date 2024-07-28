f3c4b5e583e8b287919d8127225f8ba0b5155d4d
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-07-28 12:52:36 -0400

    c

---
+++ src/data/cardsData.jsx
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        cardImg: "me-ph.png",
+        name: "Jasmine Floyd",
+        title: "Full Stack Product Innovator",
+        website: "jasminefloyd.website",
+        emailBTN: "me@me.com",
+        linkedinBTN: "https://linkedin.com",
+        about: "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.",
+        interest: "Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.",
+        qrlink: "https://link.com",
+        qrtext: "Scan QR Code to open Contact",
+        qrImg: "qr_ph.png",
+        facebook: "",
+        twitter: "",
+        instagram: "",
+        github: "",
+        linkedin: "",
+
+    },
+]
+
+// Card.propTypes = {
+//     item: PropTypes.shape({
+//         coverImg: PropTypes.string.isRequired,
+//         country: PropTypes.string.isRequired,
+//         gLink: PropTypes.string.isRequired,
+//         locationName: PropTypes.string.isRequired,
+//         travelDates: PropTypes.string.isRequired,
+//         description: PropTypes.string.isRequired
+//     }).isRequired
+// };
+
